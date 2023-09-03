8e4c9f5026da1483bfc93a7d9e537a6454d1d806
princetarapara <142597933+princetarapara@users.noreply.github.com>
2023-09-03 22:07:01 +0530

    Add files via upload

---
+++ components/Layout/Footer.js
@@ -0,0 +1,48 @@
+import React from "react";
+import InstagramIcon from "@mui/icons-material/Instagram";
+import TwitterIcon from "@mui/icons-material/Twitter";
+import GitHubIcon from "@mui/icons-material/GitHub";
+import YouTubeIcon from "@mui/icons-material/YouTube";
+import { Box, Typography } from "@mui/material";
+const Footer = () => {
+  return (
+    <>
+      <Box
+        sx={{ textAlign: "center", bgcolor: "#1A1A19", color: "white", p: 3 }}
+      >
+        <Box
+          sx={{
+            my: 3,
+            "& svg": {
+              fontSize: "60px",
+              cursor: "pointer",
+              mr: 2,
+            },
+            "& svg:hover": {
+              color: "goldenrod",
+              transform: "translateX(5px)",
+              transition: "all 400ms",
+            },
+          }}
+        >
+          {/* icons */}
+          <a href="https://www.instagram.com/krina_rudani._/"><InstagramIcon /></a>
+          <a href="https://twitter.com/home?lang=en"><TwitterIcon /></a>
+          <a href="https://github.com/"><GitHubIcon /></a>
+          <a href="https://www.youtube.com/channel/UCnKJM53mBg76a-2xdLWEBJw"><YouTubeIcon /></a>
+        </Box>
+        <Typography
+          variant="h5"
+          sx={{
+            "@media (max-width:600px)": {
+              fontSize: "1rem",
+            },
+          }}
+        >
+        </Typography>
+      </Box>
+    </>
+  );
+};
+
+export default Footer;
